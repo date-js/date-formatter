0b9ef56e5d1e0ec0d1cd582debeace5df6207ba8
Adrien Peyre <dev@adrien.peyre.fr>
2026-03-29 13:13:13 +0200

    Add docker for npm executions

---
+++ bin/docker.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+DIRECTORY=$(dirname $(realpath $0 ))
+
+TTY_FLAG=$([ -t 0 ] && echo "-it" || echo "-i")
+docker run $TTY_FLAG --rm \
+  -v "$DIRECTORY/..":/home/node/app \
+  -w /home/node/app \
+  -u $(id -u ${USER}):$(id -g ${USER}) \
+  node:24-slim \
+  "${@:-bash}"
