15bd4c8702e148efa3abd8182d865132cc7e21a1
Adrien Peyre <dev@adrien.peyre.fr>
2026-03-29 13:16:24 +0200

    Review / upgrade dependencies + add test dependency

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,25 @@
 const path = require('path');
-const TerserPlugin = require('terser-webpack-plugin');
 
-const config = {
+module.exports = {
   mode: 'production',
-  entry: {
-    'index': './src/index.ts',
-  },
+  entry: './src/index.ts',
   output: {
     filename: 'date-formatter.js',
     path: path.resolve(__dirname, 'dist'),
     libraryTarget: 'umd',
-    library: 'DateFormatter'
+    library: 'DateFormatter',
+    libraryExport: 'default',
   },
   resolve: {
-    extensions: ['.ts', '.js', '.json'],
+    extensions: ['.ts', '.js'],
   },
   module: {
     rules: [
       {
         test: /\.ts$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: 'babel-loader',
-        }
-      },
-      {
-        test: /\.ts$/,
         use: 'ts-loader',
         exclude: /node_modules/,
-      }
-    ]
-  },
-  optimization: {
-    minimize: true,
-    minimizer: [new TerserPlugin()],
+      },
+    ],
   },
 };
-
-module.exports = [config];
